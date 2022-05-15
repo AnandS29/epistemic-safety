edc18095af42c7f176f43a459021a5c2531c9b2f
AnandS29 <AnandS29@users.noreply.github.com>
2022-05-15 02:02:44 -0700

    fix

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -1,5 +1,5 @@
-for i in {24..25}
+for (( i=0; i<25; i++ ))
 do
     echo "Running example $i"
-    python3 run_example.py -e $i > log_$i.txt 2>&1 &
+    python3 run_example.py -e $i > logs/log_$i.txt 2>&1 &
 done
