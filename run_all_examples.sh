783c24ca1e7e3955c47cbbd774ad17d965d19e65
AnandS29 <AnandS29@users.noreply.github.com>
2022-05-15 02:39:53 -0700

    removed loop

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-for i in 0 ... 24
-do
-    echo "Running example $i"
-    python3 run_example.py -e $i > logs/log_$i.txt 2>&1 &
-done
+python3 run_example.py -e 0 > logs/log_0.txt 2>&1 &
+python3 run_example.py -e 1 > logs/log_1.txt 2>&1 &
+python3 run_example.py -e 2 > logs/log_2.txt 2>&1 &
+python3 run_example.py -e 3 > logs/log_3.txt 2>&1 &
+python3 run_example.py -e 4 > logs/log_4.txt 2>&1 &
+python3 run_example.py -e 5 > logs/log_5.txt 2>&1 &
+python3 run_example.py -e 6 > logs/log_6.txt 2>&1 &
+python3 run_example.py -e 7 > logs/log_7.txt 2>&1 &
+python3 run_example.py -e 8 > logs/log_8.txt 2>&1 &
+python3 run_example.py -e 9 > logs/log_9.txt 2>&1 &
+
+python3 run_example.py -e 10 > logs/log_10.txt 2>&1 &
+python3 run_example.py -e 11 > logs/log_11.txt 2>&1 &
+python3 run_example.py -e 12 > logs/log_12.txt 2>&1 &
+python3 run_example.py -e 13 > logs/log_13.txt 2>&1 &
+python3 run_example.py -e 14 > logs/log_14.txt 2>&1 &
+python3 run_example.py -e 15 > logs/log_15.txt 2>&1 &
+python3 run_example.py -e 16 > logs/log_16.txt 2>&1 &
+python3 run_example.py -e 17 > logs/log_17.txt 2>&1 &
+python3 run_example.py -e 18 > logs/log_18.txt 2>&1 &
+python3 run_example.py -e 19 > logs/log_19.txt 2>&1 &
+
+python3 run_example.py -e 20 > logs/log_20.txt 2>&1 &
+python3 run_example.py -e 21 > logs/log_21.txt 2>&1 &
+python3 run_example.py -e 22 > logs/log_22.txt 2>&1 &
+python3 run_example.py -e 23 > logs/log_23.txt 2>&1 &
+python3 run_example.py -e 24 > logs/log_24.txt 2>&1 &
